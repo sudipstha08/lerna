80821c76bb6cf30f5fe2e3c73eb49723ebcc9e79
Sudip Shrestha <sudipstha08@gmail.com>
2021-06-03 18:28:08 +0545

    feat: assets

--- dotenv.config.js
+++ dotenv.config.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv')
 const path = require('path')
+
 if (process.env.NODE_ENV === 'production') {
   dotenv.config({
     path: path.resolve(__dirname, `./.env.production`),
