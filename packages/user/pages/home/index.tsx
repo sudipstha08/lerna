8f07efc589a630400842049404c3d08d2cb844d4
Sudip Shrestha <sudipstha08@gmail.com>
2021-06-07 09:54:24 +0545

    refactor: pages

--- packages/user/pages/home/index.tsx
+++ packages/user/pages/home/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
+import { Wrapper } from './style'
 
 const HomePage = () => {
-  return <div>HomePage Index</div>
+  return (
+    <>
+      <Head>
+        <title>Home | lerna</title>
+        <meta name="description" content="homepage" />
+      </Head>
+      <Wrapper>This is the Home Page</Wrapper>
+    </>
+  )
 }
 
 export default HomePage
--- packages/user/pages/home/style.ts
+++ packages/user/pages/home/style.ts
@@ -1,3 +1,5 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  margin: 2em;
+`
